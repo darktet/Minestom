76296441257ca58771ca5cc5bc8c5980222c207b
themode <themode@outlook.fr>
2021-12-30 15:13:47 +0100

    Rename utils files

--- src/main/java/net/minestom/server/utils/PacketUtils.java
+++ src/main/java/net/minestom/server/utils/PacketUtils.java
@@ -53,9 +53,9 @@ public final class PacketUtils {
     private static final PacketListenerManager PACKET_LISTENER_MANAGER = MinecraftServer.getPacketListenerManager();
     private static final LocalCache<Deflater> LOCAL_DEFLATER = LocalCache.of(Deflater::new);
 
-    public static final boolean GROUPED_PACKET = PropertyUtil.getBoolean("minestom.grouped-packet", true);
-    public static final boolean CACHED_PACKET = PropertyUtil.getBoolean("minestom.cached-packet", true);
-    public static final boolean VIEWABLE_PACKET = PropertyUtil.getBoolean("minestom.viewable-packet", true);
+    public static final boolean GROUPED_PACKET = PropertyUtils.getBoolean("minestom.grouped-packet", true);
+    public static final boolean CACHED_PACKET = PropertyUtils.getBoolean("minestom.cached-packet", true);
+    public static final boolean VIEWABLE_PACKET = PropertyUtils.getBoolean("minestom.viewable-packet", true);
 
     /// Local buffers
     private static final LocalCache<ByteBuffer> PACKET_BUFFER = LocalCache.ofBuffer(Server.MAX_PACKET_SIZE);
